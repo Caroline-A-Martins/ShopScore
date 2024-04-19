7914a2cd44630c91595c4ca9a558696387a44aa7
Caroline-A-Martins <business.carolmartins@gmail.com>
2024-04-19 10:47:41 -0300

    Reutilizando código html da barra de menu

---
+++ js/header.js
@@ -0,0 +1,31 @@
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <!-- Inicio Navbar -->
+            <header>
+                <a href="#" class="logo">
+                    <span>ShopScore</span>
+                </a>
+                <!-- Menu-Icon -->
+                <i class='bx bx-menu' id="menu-icon"></i>
+                <!-- Links -->
+                <ul class="navbar">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="avaliacao.html">Avaliações</a></li>
+                    <li><a href="#ranking">Ranking</a></li>
+                    <li><a href="#customers">clientes</a></li>
+                </ul>
+                <!--Icons-->
+                <div class="header-icon">
+                    <i class="bx bx-search" id="search-icon"></i>
+                    <a href="pag-perfil.html"><i class='bx bx-user'></i></a>
+                </div>
+                <div class="search-box">
+                    <input type="search" name="" id="" placeholder="Pesquisa">
+                </div>
+            </header>
+            <!-- Final Navbar -->`;
+    }
+}
+
+customElements.define('custom-header', Header);
