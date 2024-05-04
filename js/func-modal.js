a697655accd1be6f6f117e818b0205588123ffdd
Caroline-A-Martins <business.carolmartins@gmail.com>
2024-05-04 14:21:12 -0300

    arrumando formulario

--- js/func-modal.js
+++ js/func-modal.js
@@ -3,52 +3,67 @@ class Modal extends HTMLElement {
         this.innerHTML = `
         <div onclick="openModal()" class="btn">+</div>
 
-        <div id="modal-container" class="modal-container">
-        <div class="modal">
-            <button class="fechar" id="fechar">X</button>
-            <h1>Inserir Avaliação</h1>
-            <form enctype="multipart/form-data" action="#">           
-                <div class="input-group">
-                    <label for="loja">Loja</label>
-                    <select id="lojas-select" onchange="getProdutos()" name="idstore">
+<div id="modal-container" class="modal-container">
+    <div class="modal">
+        <button class="fechar" id="fechar">X</button>
+        <h1>Inserir Avaliação</h1>
+        <form enctype="multipart/form-data" action="#">
+            <div class="input-group">
 
-                    </select>
+                <label for="lojas-select">Loja</label>
+                <select id="lojas-select" onchange="getProdutos()" name="idstore">
+                </select>
 
-                    <p>Não encontrou a loja? <i class='bx bxs-plus-square'  onclick="showCnpj()" style='color:#223555;font-size:25px;' ></i></p>
-                    <input type="text" id="cnpj" name="cnpj" placeholder="Insira o CNPJ..." style="display:none">
+                <label for="cnpj">Não encontrou a loja?
+                <button class="icon-wrapper" onclick="showCnpj()">
+                    <div class="text">+</div>
+                </button>
+            </label>
 
-                    <label for="produto">Produto</label>
-                    <select id="produtos-select" name="idstoreproduct">
-
-                    </select>
-
-                    <p>Não encontrou o produto? <i class='bx bxs-plus-square' style='color:#223555;font-size:25px;' onclick="showProduct()"></i></p>
-
-                    <input type="text" id="produto" name="produto" placeholder="Insira o nome do produto..." style="display:none">
-                    <input type="text" id="descricao_prod" name="descricao_prod" placeholder="Insira a descrição..." style="display:none">
-                    <input type="file" id="imagem" name="imagem" style="display:none">
-
-                    <label for="titulo">Título</label>
-                    <input type="text" id="titulo" name="titulo">
-
-                    <label for="descricao">Descrição</label>
-                    <textarea name="descricao" id="descricao" cols="50" rows="5"></textarea>
+                <div class="n-encontra">                    
+                    <input class="input-n-encontra" type="text" id="cnpj" name="cnpj" placeholder="Insira o CNPJ...">
                 </div>
-                <div class="input-group">
-                    <ul class="avaliacao">
-                        <li class="star-icon ativo" data-avaliacao="1" onclick="setNote(1)"></li>
-                        <li class="star-icon" data-avaliacao="2" onclick="setNote(2)"></li>
-                        <li class="star-icon" data-avaliacao="3" onclick="setNote(3)"></li>
-                        <li class="star-icon" data-avaliacao="4" onclick="setNote(4)"></li>
-                        <li class="star-icon" data-avaliacao="5" onclick="setNote(5)"></li>
-                    </ul>
+
+                <label for="produtos-select">Produto</label>
+                <select id="produtos-select" name="idstoreproduct"></select>
+
+                <label for="produto">Não encontrou o produto?
+                <button class="icon-wrapper" onclick="showProduct()">
+                    <div class="text">+</div>
+                </button>
+
+            </label>
+                <div class="n-encontra">                    
+                    <input type="text" id="produto" name="produto" placeholder="Insira o nome do produto...">
+
+                    <input type="text" id="descricao_prod" name="descricao_prod" placeholder="Insira a descrição..." >
+
+                    <input type="file" id="imagem" name="imagem" accept="image/*">
                 </div>
-                <input type="hidden" id="rating" name="rating" value="1">
-                <div class="input-group">
-                    <button class="btn" onclick="avaliar()">Enviar</button>
-                </div>
-            </form>
-        </div>`;
+
+                <label for="titulo">Título</label>
+                <input type="text" id="titulo" name="titulo">
+
+                <label for="descricao">Descrição</label>
+                <textarea name="descricao" id="descricao" cols="50" rows="5"></textarea>
+            </div>
+            <div class="input-group avaliacao-container">
+                <ul class="avaliacao">
+                    <li class="star-icon ativo" data-avaliacao="1" onclick="setNote(1)"></li>
+                    <li class="star-icon" data-avaliacao="2" onclick="setNote(2)"></li>
+                    <li class="star-icon" data-avaliacao="3" onclick="setNote(3)"></li>
+                    <li class="star-icon" data-avaliacao="4" onclick="setNote(4)"></li>
+                    <li class="star-icon" data-avaliacao="5" onclick="setNote(5)"></li>
+                </ul>
+            </div>
+            <input type="hidden" id="rating" name="rating" value="1">
+            <div class="input-group">
+                <button class="btn-enviar" onclick="avaliar()">Enviar</button>
+            </div>
+        </form>
+    </div>
+</div>
+`;
 
         try {
             const token = localStorage.getItem('token');
